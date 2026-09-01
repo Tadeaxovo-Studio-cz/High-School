5713687843e66d3f8aadbb1cd7f3613c662b16bc
Tadeaxovo-Studio-cz <tad.studio.cz@gmail.com>
2026-09-01 17:06:01 +0200

    Delete My First Modern Script.js

--- My First Modern Script.js
+++
@@ -1,9 +0,0 @@
-function showTime() {
-  const currentTime = document.getElementById("currentTime");
-  const now = new Date();
-
-  currentTime.textContent = now.toLocaleString("cs-CZ");
-}
-
-showTime();
-setInterval(showTime, 1000);
